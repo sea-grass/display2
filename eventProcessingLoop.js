7842ad185f9ef0143a9c08bed4cdf084afdbe2db
sea-grass <dev.seagrass@gmail.com>
2018-06-03 19:08:55 -0400

    Modify image generation and add UI interface - add ability to vary V value between pixels (using d) - add UI to modify n and d values on the fly - add prettier for js source code

--- eventProcessingLoop.js
+++ eventProcessingLoop.js
@@ -1,32 +1,37 @@
-var Rx = require('rxjs/Rx');
+var Rx = require("rxjs/Rx");
 
-function start (fps, init, update, render) {
-    'use strict';
-    const time$ = Rx.Observable.interval(1000 / fps);
-    const queue = Rx.Scheduler.queue;
-    const boardStateSubject = new Rx.Subject();
-    const animationFrameScheduler = Rx.Scheduler.animationFrame;
+function start(fps, init, update, render) {
+  "use strict";
+  const time$ = Rx.Observable.interval(1000 / fps);
+  const queue = Rx.Scheduler.queue;
+  const boardStateSubject = new Rx.Subject();
+  const animationFrameScheduler = Rx.Scheduler.animationFrame;
 
-    queue.schedule(init, 0, {
-        time: 0
-    });
+  queue.schedule(init, 0, {
+    time: 0
+  });
 
-    time$
-        .subscribe(
-        t => {
-            const state = {
-                time: t
-            };
-            queue.schedule((state) => { update(state, boardStateSubject); }, 0, state);
-        }
-        );
-    boardStateSubject
-        .subscribe(
-            state => animationFrameScheduler.schedule(state => {
-                render(state);
-            }, 0, state)
-        )
+  time$.subscribe(t => {
+    const state = {
+      time: t
+    };
+    queue.schedule(
+      state => {
+        update(state, boardStateSubject);
+      },
+      0,
+      state
+    );
+  });
+  boardStateSubject.subscribe(state =>
+    animationFrameScheduler.schedule(
+      state => {
+        render(state);
+      },
+      0,
+      state
+    )
+  );
 }
-
 
 module.exports = start;
--- index.js
+++ index.js
@@ -1,128 +1,203 @@
 const eventProcessingLoop = require("./eventProcessingLoop.js");
 
 const onBrowser = (function() {
-    var documentExists = true;
-    try {
-        console.log(document);
-    } catch (e) { documentExists = false; }
-    return documentExists;
-}());
+  var documentExists = true;
+  try {
+    console.log(document);
+  } catch (e) {
+    documentExists = false;
+  }
+  return documentExists;
+})();
 
 const fps = 60;
 const DEBUG = true;
-// keep it square
-const length = 200;
-const width = length;
-const height = length;
-const tvSize = 50;
+
 const windowWidth = 800;
 const windowHeight = 800;
 // const numTv = windowWidth/tvSize+windowHeight/tvSize;
 const numTv = 1;
 
+// keep it square
+var length = 63;
+var width = length;
+var height = length;
+var d = 0;
+
 var container;
+var canvas;
 var ctx;
 var tvList;
 
 if (onBrowser) {
-    const button = document.querySelector("button");
-    button.addEventListener('click', e => {
-        button.remove();
-        eventProcessingLoop(fps, init, update, render);
-    });
+  const button = document.querySelector("button");
+  button.addEventListener("click", e => {
+    button.remove();
+    eventProcessingLoop(fps, init, update, render);
+  });
+
+  const sizeSelector = document.querySelector("#n");
+  sizeSelector.addEventListener("change", e => {
+    const selectedValue =
+      sizeSelector.options[sizeSelector.selectedIndex].value;
+    setSize(Number.parseInt(selectedValue));
+  });
+  for (var i = length; i > 0; i--) {
+    const val = i;
+    const option = document.createElement("option");
+    option.value = val;
+    option.innerText = val;
+    sizeSelector.appendChild(option);
+  }
+
+  const differenceSelector = document.querySelector("#d");
+  differenceSelector.addEventListener("change", e => {
+    const selectedValue =
+      differenceSelector.options[differenceSelector.selectedIndex].value;
+    setDifference(Number.parseInt(selectedValue));
+  });
+  for (var i = 0; i < 10; i++) {
+    const val = Math.pow(2, i);
+    const option = document.createElement("option");
+    option.value = val;
+    option.innerText = val;
+    differenceSelector.appendChild(option);
+  }
+}
+
+function setSize(new_size) {
+  // global length, width, height, canvas
+  length = new_size;
+  width = length;
+  height = length;
+  canvas.width = width;
+  canvas.height = height;
+}
+function setDifference(new_d) {
+  // global d
+  d = new_d;
 }
 
 function init() {
-    container = document.body;
+  // global document,
+  container = document.body;
 
-    const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-    ctx = canvas.getContext("2d");
-    ctx.imageSmoothingEnabled = false;
+  canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
 
-    for (var i = 0; i < numTv; i++) {
-        const div = document.createElement("div");
-        div.classList.add("tv");
-        container.appendChild(div);
-    }
+  for (var i = 0; i < numTv; i++) {
+    const div = document.createElement("div");
+    div.classList.add("tv");
+    container.appendChild(div);
+  }
 
-    tvList = Array.from(document.querySelectorAll(".tv"));
+  tvList = Array.from(document.querySelectorAll(".tv"));
 }
 
 // perform updates to the state, then pass the updated state to the subject
 function update(state, subject) {
-    const t = state.time;
+  const t = state.time;
 
-    var board = [];
-    for (var x = 0; x < 10; x++) {
-        for (var y = 0; y < 10; y++) {
-            board.push(t % (x + y));
-        }
+  var board = [];
+  for (var x = 0; x < 10; x++) {
+    for (var y = 0; y < 10; y++) {
+      board.push(t % (x + y));
     }
+  }
 
-    subject.next(Object.assign({
+  subject.next(
+    Object.assign(
+      {
         board: board
-    }, state))
+      },
+      state
+    )
+  );
 }
 
 function render(state) {
-    // global ctx
-    const t = state.time;
-    const board = state.board;
+  // global ctx
+  const t = state.time;
+  const board = state.board;
 
-    if (DEBUG) console.time("render");
-    if (onBrowser) {
-        // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        const v = t;
-        // width or height? choose wisely. Should be square dimension canvas anyway
-        const n = width;
-        draw(n, v);
-    }
-    if (DEBUG) console.timeEnd("render");
+  if (DEBUG) console.time("render");
+  if (onBrowser) {
+    // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const v = t;
+    // width or height? choose wisely. Should be square dimension canvas anyway
+    const n = width;
+    draw(n, v);
+  }
+  if (DEBUG) console.timeEnd("render");
 }
 
 function draw(n, v) {
-    // global ctx
-    var colour = 0;
-    const weight = 1;
-    const maxColour = 256*256*256-1;
+  // global d
 
-    // draw to our canvas
-    for (var i = 0; i < n*n; i++) {
-        const coordinates = get2DFrom1D(i, n); // give it the index and column size
-        const offset = {
-            x: 0,
-            y: 0
-        };
-        const x = coordinates.x + offset.x;
-        const y = coordinates.y + offset.y;
-        const hexColour = getColourFromInteger(colour);
-        ctx.fillStyle = hexColour;
+  drawBuffer(n, generateVBuffer(n, v, d));
+}
 
-        ctx.fillRect(
-            x, y,
-            weight, weight
-        );
+// n is the size of the image, where there are n*n pixels in the image
+// v is the default variance set for this buffer
+// d is used to add a difference to the variance per pixel, based on that
+// pixel's index
+function generateVBuffer(n, v, d) {
+  const numPixels = n * n;
+  var V = new Array(numPixels);
+  // create buffer of values
+  // V[0] = initial_colour
+  // V[1..numPixels] = v at that pixel, where v=variance from the previous colour
+  // set initial colour
+  V[0] = 0;
+  for (var i = 1; i < numPixels; i++) {
+    V[i] = v + i * d - 1;
+  }
+  return V;
+}
 
-        colour = (colour+v)%maxColour;
-    }
+function drawBuffer(n, V) {
+  // global ctx
+  var colour = 0;
+  const maxColour = 256 * 256 * 256 - 1;
+  const weight = 1;
+  // draw the (kinda) buffer to our canvas
+  for (var i = 0; i < V.length; i++) {
+    colour = (colour + V[i]) % maxColour;
+    const coordinates = get2DFrom1D(i, n); // give it the index and column size
+    const offset = {
+      x: 0,
+      y: 0
+    };
+    const x = coordinates.x + offset.x;
+    const y = coordinates.y + offset.y;
+    const hexColour = getColourFromInteger(colour);
+    ctx.fillStyle = hexColour;
 
-    const imageData = ctx.canvas.toDataURL();
-    // and multiply the image to all img elements
-    tvList.forEach(tv => tv.style.background = 'url(' + imageData + ')');
+    ctx.fillRect(x, y, weight, weight);
+  }
+
+  const imageData = ctx.canvas.toDataURL();
+  // and multiply the image to all img elements
+  tvList.forEach(tv => (tv.style.background = "url(" + imageData + ")"));
 }
 
 function get2DFrom1D(index, numCols) {
-    return {
-        x: index % numCols,
-        y: Math.floor(index / numCols)
-    };
+  return {
+    x: index % numCols,
+    y: Math.floor(index / numCols)
+  };
 }
 
 // return a hex colour string from the padded hex value of an integer
 function getColourFromInteger(i) {
-    return "#" + (
-        i.toString(16).padStart(6, 0).substr(0, 6)
-    );
+  return (
+    "#" +
+    i
+      .toString(16)
+      .padStart(6, 0)
+      .substr(0, 6)
+  );
 }
